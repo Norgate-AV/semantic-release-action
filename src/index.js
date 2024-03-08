bb1598ce44eed39fb22c98d0a54763492fba30dd
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-03-08 19:25:58 +0000

    refactor: disable passed options from action

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const core = require('@actions/core');
+const core = require("@actions/core");
 const {
   handleBranchesOption,
   handleDryRunOption,
   handleCiOption,
   handleExtends,
   handleTagFormat,
-} = require('./handleOptions');
-const setUpJob = require('./setUpJob.task');
-const installSpecifyingVersionSemantic = require('./installSpecifyingVersionSemantic.task');
-const preInstall = require('./preInstall.task');
-const cleanupNpmrc = require('./cleanupNpmrc.task');
-const windUpJob = require('./windUpJob.task');
-const inputs = require('./inputs.json');
+} = require("./handleOptions");
+const setUpJob = require("./setUpJob.task");
+const installSpecifyingVersionSemantic = require("./installSpecifyingVersionSemantic.task");
+const preInstall = require("./preInstall.task");
+const cleanupNpmrc = require("./cleanupNpmrc.task");
+const windUpJob = require("./windUpJob.task");
+const inputs = require("./inputs.json");
 
 /**
  * Release main task
@@ -26,13 +26,13 @@ const release = async () => {
   await preInstall(core.getInput(inputs.extra_plugins));
   await preInstall(core.getInput(inputs.extends));
 
-  const semanticRelease = await import('semantic-release');
+  const semanticRelease = await import("semantic-release");
   const result = await semanticRelease.default({
-    ...handleBranchesOption(),
-    ...handleDryRunOption(),
-    ...handleCiOption(),
-    ...handleExtends(),
-    ...handleTagFormat()
+    // ...handleBranchesOption(),
+    // ...handleDryRunOption(),
+    // ...handleCiOption(),
+    // ...handleExtends(),
+    // ...handleTagFormat()
   });
 
   await cleanupNpmrc();
@@ -40,6 +40,6 @@ const release = async () => {
 };
 
 module.exports = () => {
-  core.debug('Initialization successful');
+  core.debug("Initialization successful");
   release().catch(core.setFailed);
 };
